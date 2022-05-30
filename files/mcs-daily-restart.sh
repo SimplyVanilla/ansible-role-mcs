1d07a685d3097625dbcb90a4b71afab1a578b366
Netherwhal <102181242+Netherwhal@users.noreply.github.com>
2022-05-30 17:14:09 +0200

    add daily-restart script (#5)
    * add daily-restart script
    
    * add condition

---
+++ files/mcs-daily-restart.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+started_seconds() {
+  local start_time="$(systemctl show mcs --property ExecMainStartTimestamp)"
+  start_time="${start_time#*=}"
+  local diff="$(( $(date "+%s") - $(date --date="${start_time}" "+%s") ))"
+
+  echo "${diff}"
+}
+
+if [[ "$(started_seconds)" -lt 86400 ]]; then
+  exit 0
+fi
+
+systemctl restart mcs
