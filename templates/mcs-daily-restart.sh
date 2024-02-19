c9c9067367891384b14fbe5fc9b3c83b7feab2e3
Netherwhal <netherwhal@simplyvanilla.net>
2024-02-20 00:41:14 +0100

    Optionally restart velocity

--- templates/mcs-daily-restart.sh
+++ templates/mcs-daily-restart.sh
@@ -3,15 +3,21 @@
 set -eo pipefail
 
 started_seconds() {
-  local start_time="$(systemctl show mcs --property ExecMainStartTimestamp)"
+  local start_time
+  start_time="$(systemctl show "${1}" --property ExecMainStartTimestamp)"
   start_time="${start_time#*=}"
   local diff="$(( $(date "+%s") - $(date --date="${start_time}" "+%s") ))"
 
   echo "${diff}"
 }
 
-if [[ "$(started_seconds)" -lt 18000 ]]; then
-  exit 0
+# Restart mcs if it has been running for more than 5 hours
+if [[ "$(started_seconds "mcs")" -gt 18000 ]]; then
+  systemctl restart mcs
 fi
 
-systemctl restart mcs
+# Restart velocity if it has been running for more than 20 hours
+if  [[ -n "$(systemctl list-units -t service --full --all --plain --no-legend velocity.service)" ]] && \
+    [[ "$(started_seconds "velocity")" -gt 72000 ]]; then
+  systemctl restart velocity
+fi
