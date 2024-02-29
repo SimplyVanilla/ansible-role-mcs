3bcf35e9b4ee1dc52bdd0aa37ab5b934fdd9f6d2
Netherwhal <netherwhal@simplyvanilla.net>
2024-03-01 00:46:35 +0100

    Make javaagent optional (#33)
    * Make javaagent optional
    
    * lint

--- MaxHeapSize.java
+++
@@ -1,6 +0,0 @@
-public class MaxHeapSize {
-  public static void main(String[] args) {
-    long maxHeapSize = Runtime.getRuntime().maxMemory();
-    System.out.println("Max heap size: " + maxHeapSize + " bytes");
-  }
-}
