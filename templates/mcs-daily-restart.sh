c3da842587600b5d0697382125a1bf563aef7e4e
Netherwhal <netherwhal@simplyvanilla.net>
2024-03-17 04:16:13 +0100

    Configurable restart threshold

--- templates/mcs-daily-restart.sh
+++ templates/mcs-daily-restart.sh
@@ -11,13 +11,13 @@ started_seconds() {
   echo "${diff}"
 }
 
-# Restart mcs if it has been running for more than 5 hours
-if [[ "$(started_seconds "mcs")" -gt 18000 ]]; then
+# Restart mcs if it has been running for more than {{ mcs_restart_threshold }} seconds
+if [[ "$(started_seconds "mcs")" -gt {{ mcs_restart_threshold }} ]]; then
   systemctl restart mcs
 fi
 
-# Restart velocity if it has been running for more than 20 hours
+# Restart velocity if it has been running for more than {{ velocity_restart_threshold }} seconds
 if  [[ -n "$(systemctl list-units -t service --full --all --plain --no-legend velocity.service)" ]] && \
-    [[ "$(started_seconds "velocity")" -gt 72000 ]]; then
+    [[ "$(started_seconds "velocity")" -gt {{ velocity_restart_threshold }} ]]; then
   systemctl restart velocity
 fi
